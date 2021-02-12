0488d95ae4aca56c12c31e2ba4f16f7084c4035d
Mousse3 <60467388+Mousse3@users.noreply.github.com>
2021-02-12 14:29:58 +0200

    Revert "Part2 valmiiksi"
    This reverts commit d9d5bc3a3ada3e506133f0aeae90ee897b4a001d.

--- part2/puhelinluettelo/src/components/App.js
+++ part2/puhelinluettelo/src/components/App.js
@@ -1,90 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Note from './Note'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
-import numberService from './ApiConnection'
-import ModifyNumber from './ModifyNumber'
-import Notification from './Notification'
-import DeleteNumber from './DeleteNumber'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+      ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter] = useState('')
-  const [ errorMessage, setErrorMessage] = useState('')
 
-  useEffect(() => {
-    numberService.getNumbers()
-      .then(numbers => {
-        setPersons(numbers)
-      })
-  }, []);
-
-  const numbersToShow = filter === ''
+  const notesToShow = filter === ''
       ? persons
-      : persons.filter(person => person.name.includes(filter));
+      : persons.filter(person => person.name.includes(filter))
 
-  const addNumber = (e) => {
+  const addNote = (e) => {
     e.preventDefault()
     if(persons.some(person => person.name === newName)) {
-        persons.some(person => {
-          if(person.name === newName) {
-            person.number = newNumber
-            ModifyNumber.modifyNumber(person)
-            setErrorMessage(`Kontaktin ${person.name} numero on muutettu`)
-          }
-        })
-    } else {
+        alert(`${newName} is already added to phonebook`)
+    }
+    else {
         const personObject = {
             name: newName ,
             number: newNumber
         }
-        numberService.create(personObject)
-          .then(returnedNumber => {
-            setPersons(persons.concat(returnedNumber))
-            setNewNumber('')
-            setNewName('')
-            setErrorMessage(`Numero lisätty`)
-            setTimeout(function() {
-              setErrorMessage()
-            }.bind(this), 4000)
-          })
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
     }
-  };
+  }
 
   const handleNameChange = (e) => {
       console.log(e.target.value)
       setNewName(e.target.value)
-  };
+  }
 
   const handleNumberChange = (e) => {
     console.log(e.target.value)
     setNewNumber(e.target.value)
-  };
+  }
 
   const handleFilterChange = (e) => {
     console.log(e.target.value)
     setFilter(e.target.value)
-  };
+  }
 
   return (
     <div>
         <h2>Phonebook</h2>
-        <Notification message={errorMessage} />
         <Filter filter={filter} filterHandler={handleFilterChange} />
         <h2>Add a new contact</h2>
-        <PersonForm noteAdder={addNumber} nameHandler={handleNameChange} numberHandler={handleNumberChange}
+        <PersonForm noteAdder={addNote} nameHandler={handleNameChange} numberHandler={handleNumberChange}
                     number={newNumber} name={newName}/>
         <h2>Numbers</h2>
-        {numbersToShow.map(noteItem => (
-            <li key={noteItem.name}>
-                {noteItem.name} {noteItem.number} 
-                <button onClick={() => DeleteNumber.removeNumber(noteItem.id, noteItem.name, {setErrorMessage})}>Delete</button>
-            </li>
-        ))}
+        <Note notes={notesToShow} />
     </div>
   )
-};
+}
 
 export default App
--- part2/puhelinluettelo/src/components/Note.js
+++ part2/puhelinluettelo/src/components/Note.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import DeleteNumber from './DeleteNumber'
 
 const Note = ({ notes }) => {
     return(
         notes.map(noteItem => (
             <li key={noteItem.name}>
-                {noteItem.name} {noteItem.number} 
-                <button onClick={() => DeleteNumber.removeNumber(noteItem.id, noteItem.name)}>Delete</button>
+                {noteItem.name} {noteItem.number}
             </li>
         ))
     )
