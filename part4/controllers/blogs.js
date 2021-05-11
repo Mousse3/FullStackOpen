bfa2e29ccb401d93578bdc76439987da78502d68
Mousse3 <tagrohn@student.uef.fi>
2021-05-11 14:36:39 +0300

    Tehtävä 4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -6,12 +6,21 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.get('/:id', async (request, response) => {
-    const blogs = await Blog.find({ _id: request.params.id })
-    response.json(blogs.map(blog => blog.toJSON()))
+blogsRouter.get('/:id', async (request, response, next) => {
+    let blog = undefined
+    try {
+        blog = await Blog.findById(request.params.id)
+    } catch (error) {
+        return next(error)
+    }
+    if (blog) {
+        return response.json(blog.toJSON())
+    } else {
+        return response.status(404).json({ error: 'Not found'})
+    }
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
     let savedBlog = undefined
     const body = request.body
 
@@ -24,26 +33,44 @@ blogsRouter.post('/', async (request, response) => {
     try {
         savedBlog = await blog.save()
     } catch (error) {
-        if (error.name === 'ValidationError') {
-            return response.status(400).json({ error: error.message })
-        }
+        return next(error)
     }
     response.status(201)
     response.json(savedBlog.toJSON())
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', async (request, response, next) => {
     let deletedBlog = undefined
     try {
         deletedBlog = await Blog.findOneAndDelete({ _id: request.params.id})
     } catch (error) {
-        if (error.name === 'CastError') {
-            return response.status(400).json({ error: 'Bad request id' })
-        }
+        return next(error)
     }
     response.status(200)
     if (deletedBlog) {
         response.json(deletedBlog.toJSON())
+    } else {
+        return response.status(404).json({ error: 'Not found' })
+    }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    let updatedBlog = undefined
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+    try {
+        updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    } catch (error) {
+        return next(error)
+    }
+    if (updatedBlog) {
+        return response.json(updatedBlog.toJSON())
     } else {
         return response.status(404).json({ error: 'Not found' })
     }
