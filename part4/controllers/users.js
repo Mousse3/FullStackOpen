7c1d7950df30136d4c7008713cde74f14b463037
Mousse3 <tagrohn@student.uef.fi>
2021-05-14 14:39:32 +0300

    Tehtävä 4.15

---
+++ part4/controllers/users.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    let savedUser = undefined
+    try {
+        savedUser = await user.save()
+    } catch (error) {
+        return next(error)
+    }
+
+    response.json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+    const blogs = await User.find({})
+    response.json(blogs.map(user => user.toJSON()))
+})
+
+module.exports = usersRouter
