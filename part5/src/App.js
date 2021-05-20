9e83dead71461039155b19c0330d9787acc29ad3
Mousse3 <tagrohn@student.uef.fi>
2021-05-20 11:59:36 +0300

    Tehtävä 5.2

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import User from './components/User'
+import LoggedUser from './components/User'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -16,6 +18,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,6 +34,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -62,10 +76,11 @@ const App = () => {
       </div>
     )
   }
-
+  
   return (
     <div>
       <h2>blogs</h2>
+      <LoggedUser key={user.id} user={user} />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/src/components/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const logOut = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload()
+}
+
+const User = ({user}) => (
+  <div>
+    <p>
+      {user.name}
+        <b> logged in </b>
+        <button onClick={logOut}>logout</button>
+    </p>
+  </div>  
+  )
+
+export default User
